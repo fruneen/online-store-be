874e8526920531182975b252916df684d422a7f0
Evgeny <fruneen@gmail.com>
2022-10-30 14:50:23 +0300

    Authorization (#5)
    * feat: add authorization service
    
    * feat: remove debug console.log

---
+++ authorization-service/src/handlers/basicAuthorizer.ts
@@ -0,0 +1,36 @@
+import { APIGatewayRequestIAMAuthorizerHandlerV2 } from 'aws-lambda';
+
+import { generatePolicy } from 'helpers/generatePolicy';
+
+const basicAuthorizer: APIGatewayRequestIAMAuthorizerHandlerV2 = (event, context, callback) => {
+  console.log('Event: ', JSON.stringify(event));
+
+  const { identitySource, routeArn } = event;
+
+  if (!identitySource) {
+    callback('Unauthorized');
+  }
+
+  try {
+    const [authorizationToken] = identitySource;
+
+    const encodedCreds = authorizationToken.split(' ')[1];
+    const buff = Buffer.from(encodedCreds, 'base64');
+    console.log(buff);
+    const plainCreds = buff.toString('utf-8').split(':');
+
+    const [username, password] = plainCreds;
+    console.log(`Username: ${username}, password: ${password}`);
+
+    const storedUserPassword = process.env[username];
+
+    const effect = !storedUserPassword || storedUserPassword !== password ? 'Deny' : 'Allow';
+    const policy = generatePolicy(encodedCreds, routeArn, effect);
+    callback(null, policy);
+  } catch (error) {
+    console.log('Error: ', error);
+    callback('Unauthorized');
+  }
+};
+
+export default basicAuthorizer;
---
+++ authorization-service/src/helpers/generatePolicy.ts
@@ -0,0 +1,13 @@
+export const generatePolicy = (principalId: string, resource: string, effect = 'Allow') => ({
+  principalId,
+  policyDocument: {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Action: 'execute-api:Invoke',
+        Effect: effect,
+        Resource: resource,
+      },
+    ],
+  },
+});
